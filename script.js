772d44be498414878587a968a082295fa782109c
David J Forer <david.j.forer@gmail.com>
2021-09-13 12:41:59 +0900

    added display to not show other classes. minor ui tweaks

--- script.js
+++ script.js
@@ -5,21 +5,30 @@ const seats = document.querySelectorAll( '.row .seat:not(.occupied)' )
 
 // const classSelect = document.getElementById('seat-class')
 
-const selectElement = document.querySelector('.seat-class')
+// const selectElement = document.querySelector('.seat-class')
 
 function showDiv(select) {
-  if(select.value=='business') {
-    document.getElementById('bc').style.display = "block";
-   }  else if (select.value=='extra') {
-    document.getElementById('extra-leg').style.display = "block";
-   } else if (select.value=='regular') {
-     document.getElementById('regular-seats').style.display = "block";
-   }
-   
-   else {
-    document.getElementById('bc').style.display = "none"
-   }
+  
+  if (select.value == "business") {
+    document.getElementById("bc").style.display = "block";
+    document.getElementById("regular-seats").style.display = "none";
+    document.getElementById("extra-leg").style.display = "none";
+  } else if (select.value == "extra") {
+    document.getElementById("extra-leg").style.display = "block";
+    document.getElementById("bc").style.display = "none";
+    document.getElementById("regular-seats").style.display = "none";
+  } else if (select.value == "regular") {
+    document.getElementById("regular-seats").style.display = "block";
+    document.getElementById("extra-leg").style.display = "none";
+    document.getElementById("bc").style.display = "none";
+  } else {
+    // document.getElementById("main-container").style.display = "none";
+    document.getElementById("regular-seats").style.display = "none";
+    document.getElementById("extra-leg").style.display = "none";
+    document.getElementById("bc").style.display = "none";
+  }
 }
+
 
 
 container.addEventListener('click', e => {
